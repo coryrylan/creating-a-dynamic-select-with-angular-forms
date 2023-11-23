ace7171098478b1900357fdcc0e2aafff55a993e
coryrylan <2021067+coryrylan@users.noreply.github.com>
2023-11-22 21:08:21 -0600

    5 files modified, 3 added, 6 deleted

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,61 @@
-import './polyfills';
+import { Component } from '@angular/core';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
+import { of } from 'rxjs';
+import 'zone.js';
 
-import { enableProdMode } from '@angular/core';
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [ReactiveFormsModule],
+  template: `
+    <form [formGroup]="form" (ngSubmit)="submit()">
 
-import { AppModule } from './app/app.module';
+      <label for="orders">Order</label>
+      <select formControlName="orders" id="orders">
+        <option *ngFor="let order of orders; let i = index" [value]="orders[i].id">
+          {{orders[i].name}}
+        </option>
+      </select>
 
-platformBrowserDynamic().bootstrapModule(AppModule).then(ref => {
-  // Ensure Angular destroys itself on hot reloads.
-  if (window['ngRef']) {
-    window['ngRef'].destroy();
+      <br><br>
+      <button>submit</button>
+    </form>
+
+  `,
+})
+export class App {
+form: FormGroup;
+  orders: any[] = [];
+
+  constructor(private formBuilder: FormBuilder) {
+    this.form = this.formBuilder.group({
+      orders: ['']
+    });
+
+    // async orders
+    of(this.getOrders()).subscribe(orders => {
+      this.orders = orders;
+      this.form.controls['orders'].patchValue(this.orders[0].id);
+    });
+
+    // synchronous orders
+    // this.orders = this.getOrders();
+    // this.form.controls.orders.patchValue(this.orders[0].id);
   }
-  window['ngRef'] = ref;
 
-  // Otherwise, log the boot error
-}).catch(err => console.error(err));
+  getOrders() {
+    return [
+      { id: '1', name: 'order 1' },
+      { id: '2', name: 'order 2' },
+      { id: '3', name: 'order 3' },
+      { id: '4', name: 'order 4' }
+    ];
+  }
+
+  submit() {
+    console.log(this.form.value);
+  }
+}
+
+bootstrapApplication(App);
